814b6c5a3def0b0114a69db3df3c4b1841e29166
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-11-23 12:09:03 +1100

    added the user, group and policy
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terraform/oci/post-config-k8s/ext-dns.tf
+++ terraform/oci/post-config-k8s/ext-dns.tf
@@ -1,12 +1,26 @@
 # IAM user with permissions to be able to update route53 records, for use with external-dns
 resource "oci_identity_user" "user_external_dns" {
   compartment_id = var.tenancy_id
-  description    = "${var.name}-external-dns"
-  name           = "${var.name}-external-dns"
+  description    = "${var.name}-user-external-dns"
+  name           = "${var.name}-user-external-dns"
   freeform_tags  = merge({ Name = "${var.name}-user-external-dns" }, var.tags)
   provider       = oci.home_region
 }
 
+resource "oci_identity_group" "group_external_dns" {
+    #Required
+    compartment_id = var.tenancy_id
+    description = "${var.name}-group-external-dns"
+    name = "${var.name}-group-external-dns"
+    freeform_tags = merge({ Name = "${var.name}-group-external-dns" }, var.tags)
+    provider       = oci.home_region
+}
+
+resource "oci_identity_user_group_membership" "external_dns_user_group_membership" {
+    group_id = oci_identity_group.group_external_dns.id
+    user_id = oci_identity_user.user_external_dns.id
+    provider       = oci.home_region
+}
 
 resource "oci_identity_api_key" "user_external_dns_api_key" {
   key_value = tls_private_key.user_external_dns_key.public_key_pem
@@ -34,9 +48,9 @@ locals {
   ### For the aws implmentation , there was one more policy which is required if we use Route53 as the DNS01 provider for cert-manger. 
   ### We dont know much about it as of now..so will go with HTTP01 challenge
   oci_external_dns_statements = [
-    "Allow ${oci_identity_user.user_external_dns.name} to manage dns-records  in compartment id ${var.compartment_id}  where target.dns-zone.id=${var.public_zone_id}",
-    "Allow ${oci_identity_user.user_external_dns.name} to manage dns-records  in compartment id ${var.compartment_id}  where target.dns-zone.id=${var.private_zone_id}",
-    "Allow ${oci_identity_user.user_external_dns.name} to inspect dns-zones in compartment id ${var.compartment_id}",
-    "Allow ${oci_identity_user.user_external_dns.name} to inspect dns-records in compartment id ${var.compartment_id}"
+    "Allow group ${oci_identity_group.group_external_dns.name} to manage dns-records  in compartment id ${var.compartment_id}  where target.dns-zone.id=${var.public_zone_id}",
+    "Allow group ${oci_identity_group.group_external_dns.name} to manage dns-records  in compartment id ${var.compartment_id}  where target.dns-zone.id=${var.private_zone_id}",
+    "Allow group ${oci_identity_group.group_external_dns.name} to inspect dns-zones in compartment id ${var.compartment_id}",
+    "Allow group ${oci_identity_group.group_external_dns.name} to inspect dns-records in compartment id ${var.compartment_id}"
   ]
 }
--- terraform/oci/post-config-k8s/longhorn_backups.tf
+++ terraform/oci/post-config-k8s/longhorn_backups.tf
@@ -13,6 +13,22 @@ resource "oci_identity_user" "longhorn_backups" {
   provider       = oci.home_region
 }
 
+resource "oci_identity_group" "longhorn_backups_group" {
+    #Required
+    compartment_id = var.tenancy_id
+    description = "${local.base_domain}-group-lhbck"
+    name = "${local.base_domain}-group-lhbck"
+    freeform_tags = merge({ Name = "${local.base_domain}-group-lhbck" }, var.tags)
+    provider       = oci.home_region
+}
+
+resource "oci_identity_user_group_membership" "longhorn_backups_user_group_membership" {
+    group_id = oci_identity_group.longhorn_backups_group.id
+    user_id = oci_identity_user.longhorn_backups.id
+    provider       = oci.home_region
+}
+
+
 resource "oci_identity_customer_secret_key" "longhorn_backups_secret_key" {
   display_name = "${local.base_domain}-lhbck-s3-keys"
   user_id      = oci_identity_user.longhorn_backups.id
@@ -36,7 +52,7 @@ locals {
 
 locals {
   oci_longhorn_backups_statements = [
-    "Allow ${oci_identity_user.longhorn_backups.name} to manage objects  in compartment id ${var.compartment_id}  where target.bucket.name=${oci_objectstorage_bucket.longhorn_backups.name}",
-    "Allow ${oci_identity_user.longhorn_backups.name} to inspect buckets in compartment id ${var.compartment_id}"
+    "Allow group ${oci_identity_group.longhorn_backups_group.name} to manage objects  in compartment id ${var.compartment_id}  where target.bucket.name=${oci_objectstorage_bucket.longhorn_backups.name}",
+    "Allow group ${oci_identity_group.longhorn_backups_group.name} to inspect buckets in compartment id ${var.compartment_id}"
   ]
 }
