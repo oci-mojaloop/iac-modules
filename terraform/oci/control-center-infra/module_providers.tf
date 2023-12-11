647dc4d9b9a9b55f33be4103bc6929028a75c529
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-12-11 15:18:57 +1100

    removed redundant entries
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terraform/oci/control-center-infra/module_providers.tf
+++ terraform/oci/control-center-infra/module_providers.tf
@@ -1,18 +1,4 @@
-# terraform {
-#   required_providers {
-#     oci = {
-#       source  = "oracle/oci"
-#       version = ">=4.67.3"
-#     }
-#   }
-#   required_version = ">= 1.0.0"
-# }
-
 provider "oci" {
   alias  = "home_region"
   region = lookup(data.oci_identity_regions.home_region.regions[0], "name")
-  # tenancy_ocid     = var.tenancy_id
-  # user_ocid        = var.user_id
-  # fingerprint      = var.api_fingerprint
-  # private_key_path = var.api_private_key_path
 }
